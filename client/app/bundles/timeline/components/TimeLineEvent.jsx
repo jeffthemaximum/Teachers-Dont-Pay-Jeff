ff0efceb1dc8a661c5df9e03df6160088a1ff733
jeffthemaximum <frey.maxim@gmail.com>
2017-03-18 12:09:48 -0400

    adds momeny js to format date

--- client/app/bundles/timeline/components/TimeLineEvent.jsx
+++ client/app/bundles/timeline/components/TimeLineEvent.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import moment from 'moment';
 
 const TimeLineEvent = React.createClass({
+  formatDate(){
+    return moment(this.props.event.date).format('MMMM Do YYYY, h:mm:ss a');
+  },
+
   render(){
     return (
       <div className="cd-timeline-block">
@@ -11,7 +16,7 @@ const TimeLineEvent = React.createClass({
         <div className="cd-timeline-content">
           <h2>{this.props.event.title}</h2>
           <p>{this.props.event.description}</p>
-          <span className="cd-date">{this.props.event.date}</span>
+          <span className="cd-date">{this.formatDate()}</span>
         </div>
       </div>
     )
