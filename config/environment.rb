233b181f7ae7f31a5449b6635e0ecdb2614055e8
jeffthemaximum <frey.maxim@gmail.com>
2017-04-23 12:53:46 -0400

    mail configs for prod

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'yourdomain.com',
+  :domain => 'www.teachersdontpayjeff.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
